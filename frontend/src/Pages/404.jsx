bfeaaa78d0ee681df0f00bb04ca7559049b2f5ae
M.T. Tasin <m.t.tasin20@gmail.com>
2024-12-12 11:33:12 +0600

    completed the 404 page

--- frontend/src/Pages/404.jsx
+++ frontend/src/Pages/404.jsx
@@ -1,10 +1,14 @@
-
+import { Link } from "react-router-dom"
 
 export default function NotFound() {
 
     return (
-        <div>
-            404
-        </div>    
+        <>
+            <div className="h-[100vh] text-white bg-black flex flex-col justify-center items-center">
+                <h1 className="text-center monoton-regular text-8xl">404</h1>
+                <p className="text-2xl">Page Not Found</p>
+                <Link to="/" className="btn mt-10 w-1/2"><button>Home</button></Link>
+            </div>
+        </> 
     )
 }
